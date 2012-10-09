18a81cea516e6514b0a68738076b3244859b6c8f
Alexander Lang <alex@upstre.am>
2012-10-09 18:25:47 +0200

    adds loading empty config instead of nil when there is no config file

--- lib/veritrans/config.rb
+++ lib/veritrans/config.rb
@@ -14,33 +14,29 @@ module Veritrans
 
     # Payment Redirect Url - defined in gem - no change!
     PAYMENT_REDIRECT_URL = "/web1/deviceCheck.action"
-    
+
     # :nodoc:
     CUSTOMER_SPECIFICATION_FLAG = '0' #Billing same as shipping address '1' Different, manually input in Veritrans-web
 
     # Default Settlement method:
     SETTLEMENT_TYPE_CARD = "01" #Paymanet Type
-    
+
     # Flag: Sales and Sales Credit, 0: only 1 credit. If not specified, 0
     CARD_CAPTURE_FLAG = "1"
 
-    def Config.included(mod)
-      class <<self
+    def self.included(mod)
+      mod.class_eval do
         @@config_env = ::Object.const_defined?(:Rails) ? Rails.env : "development"
-        @@config = File.exists?("./config/veritrans.yml") ? YAML.load_file("./config/veritrans.yml") : nil
+        @@config = File.exists?("./config/veritrans.yml") ? YAML.load_file("./config/veritrans.yml") : {@@config_env => {}}
+
+        def self.config_env=(env)
+          @@config_env = env
+        end
+
+        def self.config
+          @@config[@@config_env]
+        end
       end
-
-      mod.instance_eval <<CODE
-
-      def self.config_env=(env)
-        @@config_env = env
-      end
-
-      def self.config
-        @@config[@@config_env]
-      end 
-CODE
-
     end
   end
 end
--- spec/unit/veritrans/config_spec.rb
+++ spec/unit/veritrans/config_spec.rb
@@ -11,7 +11,7 @@ describe Veritrans::Config do
       klass = nil
       if Object.const_defined?(:Rails)
         klass = Rails
-        Object.send(:remove_const, :Rails) 
+        Object.send(:remove_const, :Rails)
       end
       Tmp_class.send(:include, Veritrans::Config)
       Tmp_class.class_eval("@@config_env").must_equal('development')
@@ -19,18 +19,35 @@ describe Veritrans::Config do
     end
 
     it "return mode from Rails.env" do
-      klass = nil 
+      klass = nil
       if !::Object.const_defined?(:Rails)
-        klass = Class.new 
+        klass = Class.new
         Rails = klass
         def Rails.env
         end
       end
-      Rails.stub(:env, 'production') do 
+      Rails.stub(:env, 'production') do
         Tmp_class.send(:include, Veritrans::Config)
         Tmp_class.class_eval("@@config_env").must_equal('production')
       end
       Object.send(:remove_const, :Rails) if klass
+    end
+
+    describe 'when there is no context file' do
+      __DIR__ = File.dirname(__FILE__)
+      before(:each) do
+        FileUtils.mv __DIR__ + '/../../../config/veritrans.yml', __DIR__ + '/../../../config/veritrans.yml.moved'
+      end
+
+      after(:each) do
+        FileUtils.mv __DIR__ + '/../../../config/veritrans.yml.moved', __DIR__ + '/../../../config/veritrans.yml'
+      end
+
+      it 'returns an empty config if no config file found' do
+        Tmp_class.send(:include, Veritrans::Config)
+
+        Tmp_class.config.must_equal({})
+      end
     end
   end
 end
